6ab4cf892549045641c3d6ed81237aa33bb7246e
bren0r <nuriomonov1@mail.ru>
2023-07-25 22:52:00 +0300

    first view for index.html

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+
+def index(request):
+    """
+    Функция отображения для домашней страницы
+
+    """
+
+    num_books = Book.objects.all().count()
+    num_instance = BookInstance.objects.all().count()
+
+    # Доступные книги (статус = 'а')
+
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+
+    # Отрисовка html- шаблона index.html с данными внутри
+    # переменной контекста context
+
+    return render(
+        request,
+        'index.html',
+        context=
+        {'num_books': num_books, 'num_instance': num_instance, 'num_instances_available': num_instances_available,
+         'num_authors': num_authors},
+    )
