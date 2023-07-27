4bae8b94bbfd92e5115f38dc5cd356758fc7f543
bren0r <nuriomonov1@mail.ru>
2023-07-27 22:12:25 +0300

    ctrl + alt + l pep8)

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -65,10 +65,8 @@ class Author(models.Model):
     date_of_birth = models.DateField(null=True, blank=True)
     date_of_death = models.DateField('Died', null=True, blank=True)
 
-
     def get_absolute_url(self):
         return reverse('author-detail', args=[str(self.id)])
-
 
     def __str__(self):
         return '{0}, {1}'.format(self.last_name, self.first_name)
